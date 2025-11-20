439a62ef5f3cdf8757b81699f1f2d9fd08a90a80
Paolo Vacinaletti <vacinalettipaolo93@gmail.com>
2025-11-20 13:25:27 +0100

    Delete functions directory

--- functions/api/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/**
- * Wrapper: re-export the compiled function in ../lib/index.js
- * This allows the emulator (which loads source "functions/api") to run the code
- * that lives in functions/lib/index.js (il JS compilato).
- */
-const mod = require('../lib/index.js');
-exports.api = mod.api || (mod && mod.default && mod.default.api) || mod;
