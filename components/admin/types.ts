8665b74ca91455edd591bd88b21f22a0e1291dac
Paolo Vacinaletti <vacinalettipaolo93@gmail.com>
2025-11-20 13:21:24 +0100

    Delete components directory

--- components/admin/types.ts
+++
@@ -1,9 +0,0 @@
-
-import { type AppSettings } from '../../types';
-
-export type Tab = 'bookings' | 'settings' | 'personalization' | 'integrations';
-
-export interface TabProps {
-    settings: AppSettings;
-    onSettingsChange: () => void;
-}
