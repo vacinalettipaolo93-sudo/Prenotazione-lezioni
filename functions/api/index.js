817132bfa69bab6cb1d1486340507b557909bb2d
Paolo Vacinaletti <vacinalettipaolo93@gmail.com>
2025-11-20 10:41:51 +0100

    Remove commented instructions in index.js

--- functions/api/index.js
+++ functions/api/index.js
@@ -3,10 +3,6 @@
  * Wrapper: re-export the compiled function in ../lib/index.js
  * This allows the emulator (which loads source "functions/api") to run the code
  * that lives in functions/lib/index.js (il JS compilato).
- *
- * Salva questo file in: functions/api/index.js
  */
 const mod = require('../lib/index.js');
-
-// Se il modulo compilato esporta "api" direttamente
 exports.api = mod.api || (mod && mod.default && mod.default.api) || mod;
